91005b461394652909bdf4312de41e92dc4697c3
saranam <hjk786@naver.com>
2022-07-22 15:59:03 +0900

    #3.1 Call Controls - 비디오 device 선택하기 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,10 +3,29 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 // user로부터 온 오디오와 비디오가 결합된 stream
 let myStream;
 let muted = false;
 let cameraOff = false;
+//
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    // console.log(devices); // -> devices가 array로 출력됨
+    // kind 가 videoinput인 것만 필요
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    console.log(cameras);
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      camerasSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 async function getMedia() {
   try {
@@ -16,6 +35,7 @@ async function getMedia() {
     });
     // console.log(myStream);
     myFace.srcObject = myStream;
+    await getCameras();
   } catch (error) {
     console.log(error);
   }
@@ -23,6 +43,9 @@ async function getMedia() {
 getMedia();
 // 음소거 버튼 클릭시 발생
 function handleMuteClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (!muted) {
     muteBtn.innerText = "Unmute";
     muted = true;
@@ -33,6 +56,9 @@ function handleMuteClick() {
 }
 // 카메라 버튼 클릭시 발생
 function handleCameraClick() {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (cameraOff) {
     cameraBtn.innerText = "Turn Camera Off";
     cameraOff = false;
